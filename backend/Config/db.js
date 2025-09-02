55afdc9f733392e4d313b08be110e9c1595c9d40
Tri Dung <tranductridung0103@gmail.com>
2025-09-02 10:34:37 +0700

    update config dataase

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -5,9 +5,11 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USENAME,
   process.env.DATABASE_PASSWORD,
   {
-    host: "localhost",
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: "mysql",
   }
 );
 
 module.exports = sequelize;
+
